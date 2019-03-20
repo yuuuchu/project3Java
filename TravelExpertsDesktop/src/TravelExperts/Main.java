1e77f7125d8349169d81f1e6397a327198b7ab47
heinerom <44779365+heinerom@users.noreply.github.com>
2019-03-20 16:37:27 -0600

    pic

--- TravelExpertsDesktop/src/TravelExperts/Main.java
+++ TravelExpertsDesktop/src/TravelExperts/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 793, 440));
         primaryStage.show();
