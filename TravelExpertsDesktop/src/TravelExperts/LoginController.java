8f399923fb80e84d50d6a485d3ec36a9425b288f
heinerom <44779365+heinerom@users.noreply.github.com>
2019-03-27 15:43:39 -0600

    mmm

---
+++ TravelExpertsDesktop/src/TravelExperts/LoginController.java
@@ -0,0 +1,67 @@
+package TravelExperts;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label lblTravelExperts;
+
+    @FXML
+    private Label lblUser;
+
+    @FXML
+    private TextField txtUserField;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    void OnClicked(MouseEvent event) {
+        // write your code here
+        DBLoginConnection loginObj=new DBLoginConnection();
+
+
+        if(loginObj.loginUser("Muhammad","Khan")){
+            lblUser.setText("Valid");
+        }
+        else{
+            lblUser.setText("Invalid");
+        }
+    }
+
+
+
+
+
+    @FXML
+    void initialize() {
+        assert lblTravelExperts != null : "fx:id=\"lblTravelExperts\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert lblUser != null : "fx:id=\"lblUser\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert txtUserField != null : "fx:id=\"txtUserField\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert lblPassword != null : "fx:id=\"lblPassword\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert txtPassword != null : "fx:id=\"txtPassword\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert btnSignIn != null : "fx:id=\"btnSignIn\" was not injected: check your FXML file 'LoginPage.fxml'.";
+
+    }
+}
--- TravelExpertsDesktop/src/TravelExperts/Main.java
+++ TravelExpertsDesktop/src/TravelExperts/Main.java
@@ -4,10 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Button btnSignIn;
+    TextField txtUserField;
+    PasswordField txtPassword;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
@@ -18,6 +24,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+
     }
 }
