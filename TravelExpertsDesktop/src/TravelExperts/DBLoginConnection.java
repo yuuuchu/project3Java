d5e4e8f9c99d1f1c8dbd3d60c45d728483342a9a
yuuuchu <44778601+yuuuchu@users.noreply.github.com>
2019-04-05 08:30:59 -0600

    .

--- TravelExpertsDesktop/src/TravelExperts/DBLoginConnection.java
+++ TravelExpertsDesktop/src/TravelExperts/DBLoginConnection.java
@@ -12,7 +12,7 @@ public class DBLoginConnection {
         {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/travelexperts", "root", "");
+                    "jdbc:mysql://localhost:3306/travelexperts", "user1", "");
             stmt = con.createStatement();
 
         }
